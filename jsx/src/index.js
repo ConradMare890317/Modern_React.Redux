4f71e624e7bfbec927583e0dd64112f48fb47048
ConradMare890317 <Conradmare@gmail.com>
2021-05-31 17:34:24 +0200

    2021/05/31 (pm) JSX vs. HTML

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,8 +1,28 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+    module.hot.accept();
+}
 
 // Create a react component
+const App = () => {
+    const buttonText = 'Click Me!';
 
+    return (
+        <div>
+            <label className="label" for="name">
+                Hello React-World! Ready?
+            </label>
 
-// Take the react component and display it on the screen
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'black', color: 'white' }}>
+                {buttonText}
+            </button>
+        </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));

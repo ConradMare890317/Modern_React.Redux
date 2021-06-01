e6c31f5db0083a9ac80267db597acf3a1e13aa8c
ConradMare890317 <Conradmare@gmail.com>
2021-06-01 12:51:32 +0200

    2021/06/01 (pm) New app - 'components.'

---
+++ components/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+const App = () => {
+    return <div>Hello React-World!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));

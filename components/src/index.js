dcdb9628f8fb4aec79cb56e0b901afb56c3243e9
ConradMare890317 <Conradmare@gmail.com>
2021-06-02 11:07:40 +0200

    2021/06/02 (am) Approval Card

--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
+
+if (module.hot) {
+    module.hot.accept();
+}
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Conrad" timeAgo="Today at 16:00" content="Hello" />
-            <CommentDetail author="Phil" timeAgo="Today at 16:30" content="React" />
-            <CommentDetail author="Mariaan" timeAgo="Yesterday at 12:00" content="World!" />
-            <CommentDetail author="Celesté" timeAgo="Monday at 09:00" content="Are You" />
-            <CommentDetail author="Ruan" timeAgo="Today at 06:00" content="Ready?" />
+            <ApprovalCard />
+            <CommentDetail
+                author="Conrad"
+                timeAgo="Today at 16:00"
+                content="Hello"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Phil"
+                timeAgo="Today at 16:30"
+                content="React"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Mariaan"
+                timeAgo="Yesterday at 12:00"
+                content="World!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Celesté"
+                timeAgo="Monday at 09:00"
+                content="Are You"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Ruan"
+                timeAgo="Today at 06:00"
+                content="Ready?"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };

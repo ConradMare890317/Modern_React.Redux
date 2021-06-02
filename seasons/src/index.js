37fdc0ee5e6aa34949d3241f985d402f56d72344
ConradMare890317 <Conradmare@gmail.com>
2021-06-02 16:19:12 +0200

    2021/06/02 (pm) Refracturing from Functional to Class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from "react-dom";
 if (module.hot) {
     module.hot.accept();
 }
-
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-
-    return <div>Hi there!</div>;
-};
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 

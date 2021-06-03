704b48117a3bddb9ed54f416520cc7d8fc67d0d8
ConradMare890317 <Conradmare@gmail.com>
2021-06-03 15:43:40 +0200

    2021/06/03 (pm) Icons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,46 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-if (module.hot) {
-    module.hot.accept();
-}
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        //THIS IS THE ONLY TIME we do direct assignment to
-        // this.state!!!
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                //Called state!
-                this.setState({ lat: position.coords.latitude });
-
-                //WE DID NOT!!!
-                // this.state.lat = position.coords.latitude
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
-    // React requires render!
     render() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <div>Loading!</div>;
     }
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 
 // GeoLocation API = "developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/"

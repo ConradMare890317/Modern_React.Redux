73254de1578b39fa52a56e883261a05746859dc8
ConradMare890317 <Conradmare@gmail.com>
2021-06-03 18:39:57 +0200

    2021/06/03 (pm) Styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,24 +1,34 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Winter is Coming.',
+        iconName: 'snowflake'
+    }
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat < 0 ? 'winter' : 'summer';
+        return lat > 0 ? 'winter' : 'summer';
     }
 };
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text =
-        season === 'winter' ? '"Ice, Ice Baby!"' : '"Hot stuff!"';
-    const icon = season === 'winter' ? 'snowflake outline' : 'sun outline';
+    const { text, iconName } = seasonConfig[season];
 
     return (
-        <div>
-            <i className={`${icon} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left huge ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${icon} icon`} />
+            <i className={`icon-right huge ${iconName} icon`} />
         </div>
     );
 };
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
 

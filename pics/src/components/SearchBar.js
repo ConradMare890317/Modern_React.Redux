e849298f2b04274f3caf1e5112e1587b01147063
ConradMare890317 <Conradmare@gmail.com>
2021-06-05 14:51:21 +0200

    2021/06/05 (pm) Callbacks in Children.

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,12 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = event => {
         event.preventDefault();
 
-        //Note!: Section 7 - Video 90!!!!! ('this' in JavaScript.)
-        console.log(this.state.term);
-    }
+        this.props.onSubmit(this.state.term);
+    };
 
     render() {
         return (

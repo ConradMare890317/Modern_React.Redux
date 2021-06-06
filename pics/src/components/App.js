beef0c85e152c9f90593c8ba868879fd2584a2a9
ConradMare890317 <Conradmare@gmail.com>
2021-06-06 11:33:30 +0200

    2021/06/06 (am) Rendering Lists

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization:
-                    'Client-ID PVP3mBs3BvbeBooJKQ8S_eAX06GlHYadWfsVT95xtRY'
-            }
+    state = { images: [] };
+
+    onSearchSubmit = async term => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term }
         });
+
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '20px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         );
     }

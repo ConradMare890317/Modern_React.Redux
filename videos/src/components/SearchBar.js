33ef736581b4392ffdbeeb4cf2f5f0acc9a030ba
ConradMare890317 <Conradmare@gmail.com>
2021-06-08 10:43:38 +0200

    2021/06/08 (am) Rendering a List of Videos

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: "" };
+    state = { term: '' };
 
     onInputChange = event => {
         this.setState({ term: event.target.value });
@@ -11,16 +11,16 @@ class SearchBar extends React.Component {
         event.preventDefault();
 
         this.props.onFormSubmit(this.state.term);
-    }
+    };
 
     render() {
         return (
-            <div className='search-bar ui segment'>
-                <form onSubmit={this.onFormSubmit} className='ui form'>
-                    <div className='field'>
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
                         <label>Video Search</label>
                         <input
-                            type='text'
+                            type="text"
                             value={this.state.term}
                             onChange={this.onInputChange}
                         />

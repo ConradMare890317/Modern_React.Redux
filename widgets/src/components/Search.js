e59f5ce3ea1b9f15a309f786ae6aba67b6adcb99
ConradMare890317 <Conradmare@gmail.com>
2021-06-23 12:15:39 +0200

    2021/06/23 (am) Reminder on setTimeout
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -7,33 +7,42 @@ const Search = () => {
 
   useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', { 
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
           srsearch: term,
-        }
+        },
       });
 
       setResults(data.query.search);
-
     };
 
-    search();
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
-    <div key={result.pageid} className="item">
-      <div className="content">
-        <div className="title">
-          {result.title}
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
-        {result.snippet}
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
       </div>
-    </div>
     );
   });
 

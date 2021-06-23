ad53179d8fd2635a83a0bec29b67a2cd28e2c93c
ConradMare890317 <Conradmare@gmail.com>
2021-06-23 10:20:34 +0200

    2021/06/23 (am) List Building
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,15 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('Default');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('asldkfj');
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', { 
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        }
+      });
+
+      setResults(data.query.search);
+
     };
 
     search();
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+    <div key={result.pageid} className="item">
+      <div className="content">
+        <div className="title">
+          {result.title}
+        </div>
+        {result.snippet}
+      </div>
+    </div>
+    );
+  });
 
   return (
     <div>
@@ -24,6 +49,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };

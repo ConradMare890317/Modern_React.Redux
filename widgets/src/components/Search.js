58daaac27111083deff62e3ea08dd0b000f977f5
ConradMare890317 <Conradmare@gmail.com>
2021-06-23 09:36:08 +0200

    2021/06/23 (am) Async Code in useEffect
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
 
-  console.log('I run with every render.')
-
   useEffect(() => {
-    console.log('I run after every render.');
+    const search = async () => {
+      await axios.get('asldkfj');
+    };
+
+    search();
   }, [term]);
 
-    return (
-      <div>
-        <div className="ui form">
-          <div className="field">
-            <label>Enter Search Term</label>
-            <input 
-              value={term}
-              onChange={e => setTerm(e.target.value)}
-              className="input"
-            />
-          </div>
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Search;

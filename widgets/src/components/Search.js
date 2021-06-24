d63291eb06c93aa8fa731414628f21b427dcebdb
ConradMare890317 <Conradmare@gmail.com>
2021-06-24 10:19:48 +0200

    2021/06/24 (am) Dropdown for Wikipedia API app
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('Default');
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term,
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
+    if (debouncedTerm) {
       search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
     }
-  }, [term, results.length]);
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
@@ -43,7 +43,7 @@ const Search = () => {
             className="ui button"
             href={`https://en.wikipedia.org?curid=${result.pageid}`}
           >
-            Select
+            Go
           </a>
         </div>
         <div className="content">
@@ -58,7 +58,7 @@ const Search = () => {
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Search</label>
+          <label>Enter Search Term</label>
           <input
             value={term}
             onChange={(e) => setTerm(e.target.value)}

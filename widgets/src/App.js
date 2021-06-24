5a0a17f553280e7228f015b51d536d552f2e7f12
ConradMare890317 <Conradmare@gmail.com>
2021-06-24 11:07:38 +0200

    2021/06/24 (am) Selection State
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+import React, { useState } from "react";
+//import Accordion from "./components/Accordion";
+//import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 
-const items = [
+/* const items = [
   {
     title: "What is React?",
     content: "React is a front end javascript framework",
@@ -16,12 +16,33 @@ const items = [
     title: "How do you use React?",
     content: "You use React by creating components",
   },
+]; */
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };

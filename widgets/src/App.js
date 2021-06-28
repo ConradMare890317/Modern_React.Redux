cc1f5f22001f67d16a346b2e262d2e4471d620cd
ConradMare890317 <Conradmare@gmail.com>
2021-06-28 12:29:04 +0200

    2021/06/28 (pm) Translate.app/ google-translate.API
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-//import Accordion from "./components/Accordion";
-//import Search from "./components/Search";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
-/* const items = [
+const items = [
   {
     title: "What is React?",
     content: "React is a front end javascript framework",
@@ -16,7 +17,7 @@ import Dropdown from "./components/Dropdown";
     title: "How do you use React?",
     content: "You use React by creating components",
   },
-]; */
+];
 
 const options = [
   {
@@ -34,15 +35,9 @@ const options = [
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-
   return (
     <div>
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <Translate />
     </div>
   );
 };

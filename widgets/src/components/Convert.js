30e2001f4450a6abb32ad157f5ba0dbb0c9099b9
ConradMare890317 <Conradmare@gmail.com>
2021-06-28 14:12:56 +0200

    2021/06/28 (pm)
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -2,32 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-	const [ translated, setTranslated ] = useState('');
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
 
-	useEffect(
-		() => {
-			const doTranslation = async () => {
-				const { data } = await axios.post(
-					'https://translation.googleapis.com/language/translate/v2',
-					{},
-					{
-						params: {
-							q: text,
-							target: language.value,
-							key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-						},
-					}
-				);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
 
-        setTranslated(data.data.translations[0].translatedText);
-			};
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
 
-      doTranslation();
-		},
-		[ language, text ]
-	);
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
 
-	return (
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
     <div>
       <h1 className="ui header">{translated}</h1>
     </div>
--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -29,7 +29,7 @@ const Translate = () => {
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Enter Text</label>
+          <label>Text to Translate</label>
           <input value={text} onChange={(e) => setText(e.target.value)} />
         </div>
       </div>
@@ -40,7 +40,7 @@ const Translate = () => {
         options={options}
       />
       <hr />
-      <h3 className="ui header">Output</h3>
+      <h3 className="ui header">Translation:</h3>
       <Convert text={text} language={language} />
     </div>
   );

9df3b97eff3901a298e6613aaefaa6d4cf811b09
ConradMare890317 <Conradmare@gmail.com>
2021-07-06 16:45:15 +0200

    2021/07/06 (pm) Widgets App resumed.
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -29,7 +29,7 @@ const Translate = () => {
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Text to Translate</label>
+          <label>Enter Text</label>
           <input value={text} onChange={(e) => setText(e.target.value)} />
         </div>
       </div>
@@ -40,7 +40,7 @@ const Translate = () => {
         options={options}
       />
       <hr />
-      <h3 className="ui header">Translation:</h3>
+      <h3 className="ui header">Output</h3>
       <Convert text={text} language={language} />
     </div>
   );

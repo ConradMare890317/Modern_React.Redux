a5d7680545ad31c28645d007859bae15840d0bd1
ConradMare890317 <Conradmare@gmail.com>
2021-07-18 11:56:28 +0200

    2021/07/18 (am) Switch Statement, Display List.
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,14 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-
-// Bad practice!!!!
-
-// export const fetchPosts = async () => {
-//  const response = await jsonPlaceholder.get('./posts');
-
-
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('./posts');
+  const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response })
-  };
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};

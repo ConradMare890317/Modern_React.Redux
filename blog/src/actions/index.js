dda608c1a1668cc726c744ec841ef66211e3587a
ConradMare890317 <Conradmare@gmail.com>
2021-07-20 10:16:37 +0200

    2021/07/20 (am)
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -23,6 +23,9 @@ export const fetchUser = id => async dispatch => {
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
 
+
+// Above "const fetchUser" is Refractored.
+
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 // const _fetchUser = _.memoize(async (id, dispatch) => {
 //   const response = await jsonPlaceholder.get(`/users/${id}`);
